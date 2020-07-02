c331201ba03673ec537ca82924eebdf289186703
Nytch <ken@localhost.localdomain>
2020-07-02 19:57:04 +0300

    Changing the boiler plate

--- pages/index.js
+++ pages/index.js
@@ -1,61 +1,39 @@
+import React,{ useEffect, useState } from 'react';
 import Head from 'next/head'
 
 export default function Home() {
+    const [active,setActive] = useState()
+    const careers = ['Developers','Designers','Bloggers','Artists','Awesomeness']
+
+    useEffect(() => {
+        let counter = 0;
+        const setCounter = () => {
+            counter++;
+            if (counter == careers.length) counter = 0;
+        }
+        const interval = setInterval(() => {
+            setActive(careers[counter]);
+            setCounter()
+        }, 1300)
+        return () => clearInterval(interval);
+    },[])
   return (
     <div className="container">
       <Head>
-        <title>Create Next App</title>
+        <title>Creative Community</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+          <h1 className='title'>1010</h1>
+            <code>{active}</code>
       </main>
 
       <footer>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+            href="https://github.com/1010-C"
         >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
+          <img src="/github.svg" alt="github" className="logo" />
         </a>
       </footer>
 
@@ -77,6 +55,11 @@ export default function Home() {
           justify-content: center;
           align-items: center;
         }
+        .title {
+            margin: 0;
+            font-size: 100px;
+            letter-spacing: 20px;
+        }
 
         footer {
           width: 100%;
@@ -90,6 +73,15 @@ export default function Home() {
         footer img {
           margin-left: 0.5rem;
         }
+        .logo{
+            height: 26px;
+            width: 26px;
+            transition: .3s ease;
+        }
+        .logo:hover{
+            height: 32px;
+            width: 32px;
+        }
 
         footer a {
           display: flex;
@@ -102,84 +94,13 @@ export default function Home() {
           text-decoration: none;
         }
 
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
         code {
           background: #fafafa;
           border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
+          padding: 10px;
+          font-size: 25px;
           font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
             DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
         }
 
         @media (max-width: 600px) {
